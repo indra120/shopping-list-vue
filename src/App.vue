646f6afea5d9412224bd13c5218b6c90417dd7e6
indra120 <indrapranata120@gmail.com>
2024-02-06 17:15:25 +0800

    Displaying the statistic of list

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     className="flex flex-col h-[540px] max-w-[480px] p-4 my-6 mx-auto bg-gray-100 rounded-xl shadow-md"
   >
     <AddShoppingItem :shopping-list="shoppingList" />
+    <Info :shopping-list="shoppingList" @delete-all="deleteAllItems" />
     <ShoppingLists :shopping-list="shoppingList" />
   </section>
 </template>
@@ -15,10 +16,15 @@ import { ShoppingItem } from "./types"
 import Header from "./components/Header.vue"
 import ShoppingLists from "./components/ShoppingLists.vue"
 import AddShoppingItem from "./components/AddShoppingItem.vue"
+import Info from "./components/Info.vue"
 
 const shoppingList = ref<ShoppingItem[]>([
   { title: "Susu ultra", count: 1 },
   { title: "Tahu sumedang", count: 1 },
   { title: "Semangka", count: 1 },
 ])
+
+const deleteAllItems = () => {
+  shoppingList.value = []
+}
 </script>
---
+++ src/components/Info.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex justify-between items-center w-full py-4 px-0">
+    <div class="flex justify-center items-center text-gray-700 text-xs">
+      <p class="font-bold">Total List: {{ shoppingList.length }}</p>
+    </div>
+
+    <div class="flex justify-center items-center text-gray-700 text-xs">
+      <p class="font-bold">Total Counts: {{ totalCounts }}</p>
+    </div>
+
+    <button
+      @click="emit('deleteAll')"
+      class="bg-[none] border-0 p-0 text-red-500 text-xs font-bold cursor-pointer"
+    >
+      Delete All Items
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+import { ShoppingItem } from "../types"
+
+const props = defineProps<{ shoppingList: ShoppingItem[] }>()
+const emit = defineEmits<{ deleteAll: [] }>()
+
+const totalCounts = computed(() => {
+  return props.shoppingList.reduce((acc, curr) => acc + curr.count, 0)
+})
+</script>
