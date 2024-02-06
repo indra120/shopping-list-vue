2bee70a524a38544964d50b77c83f78f84d94a1c
indra120 <indrapranata120@gmail.com>
2024-02-06 16:26:16 +0800

    Increment & decrement amount of item

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="border-0 bg-[unset] p-[unset] size-6 cursor-pointer">
+    <img :src="icon" :alt="alt" />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{ icon: string; alt: string }>()
+</script>
--- src/components/ShoppingLists.vue
+++ src/components/ShoppingLists.vue
@@ -4,16 +4,46 @@
   >
     <div
       class="flex justify-between items-center h-14 py-3 px-4 bg-white text-gray-700 text-base font-semibold"
+      :class="shoppingList.length === i + 1 && 'border-b border-gray-100'"
       v-for="(item, i) in shoppingList"
       :key="i"
     >
       {{ item.title }}
+      <div className="flex items-center justify-between w-32">
+        <div
+          className="flex justify-center items-center w-8 h-6 rounded text-indigo-500 bg-gray-100 shadow-md"
+        >
+          {{ item.count }}
+        </div>
+
+        <ActionButton
+          @click="decrement(i)"
+          :icon="minusIcon"
+          alt="minus icon"
+        />
+        <ActionButton
+          @click="shoppingList[i].count++"
+          :icon="plusIcon"
+          alt="plus icon"
+        />
+      </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { ShoppingItem } from "../types"
+import ActionButton from "../components/ActionButton.vue"
+import plusIcon from "../assets/plus-icon.svg"
+import minusIcon from "../assets/minus-icon.svg"
 
-defineProps<{ shoppingList: ShoppingItem[] }>()
+const props = defineProps<{ shoppingList: ShoppingItem[] }>()
+
+const decrement = (i: number) => {
+  if (props.shoppingList[i].count > 0) {
+    props.shoppingList[i].count--
+  } else {
+    props.shoppingList.splice(i, 1)
+  }
+}
 </script>
